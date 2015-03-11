5dabbd711624b5b89e3b8ad632bd19eb6c2f8b52
Kubo2 <kelerest123@gmail.com>
2015-03-11 23:50:54 +0100

    Add command-line flag for entering test mode
    either -t or --test can be used to switch into test mode

--- bin/composer-cleaner.php
+++ bin/composer-cleaner.php
@@ -7,5 +7,7 @@ set_exception_handler(function($e) {
 	exit(1);
 });
 
-$cleaner = new Cleaner;
-$cleaner->clean(isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : getcwd());
+$argv = array_slice($_SERVER['argv'], 1); // indexed from 0
+
+$cleaner = new Cleaner($testMode = isset($argv[0]) && ($argv[0] === '-t' || $argv[0] === '--test'));
+$cleaner->clean(isset($argv[0]) && !$testMode ? $argv[0] : (isset($argv[1]) ? $argv[1] : getcwd()));
