0b2a0088c96f7a5e21ceeddcd8a2992f839767a9
David Grudl <david@grudl.com>
2018-12-06 16:41:15 +0100

    added .phpstorm.meta.php to $alwaysIgnore

--- src/ComposerCleaner/Cleaner.php
+++ src/ComposerCleaner/Cleaner.php
@@ -29,7 +29,7 @@ class Cleaner
 	private static $allowedComposerTypes = [null, 'library', 'composer-plugin'];
 
 	/** @var string[] */
-	private static $alwaysIgnore = ['composer.json', 'license*', 'LICENSE*'];
+	private static $alwaysIgnore = ['composer.json', 'license*', 'LICENSE*', '.phpstorm.meta.php'];
 
 
 	public function __construct(IOInterface $io, Filesystem $fileSystem)
